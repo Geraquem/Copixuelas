3a1481c92e9ac1ee1a6892a145c8c918a11ec985
Geraquem <geraquem@gmail.com>
2023-05-30 20:00:11 +0200

    wip

--- app/src/main/java/com/mmfsin/copixuelas/presentation/moneda/MonedaHelper.kt
+++ app/src/main/java/com/mmfsin/copixuelas/presentation/moneda/MonedaHelper.kt
@@ -4,8 +4,10 @@ import com.mmfsin.copixuelas.presentation.moneda.CoinResult.CARA
 import com.mmfsin.copixuelas.presentation.moneda.CoinResult.CRUZ
 
 fun getCoinResult(): CoinResult {
-    val rand = (0..10).random()
-    return if (rand % 2 == 0) CRUZ else CARA
+    // 1 2 3 4 5 6 7 - 8 9 10 11 12
+    val rand = (1..12).random()
+//    return if (rand % 2 == 0) CRUZ else CARA
+    return if (rand in 1..7) CRUZ else CARA
 }
 
 enum class CoinResult {
