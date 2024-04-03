40055b5d629710d609bef3da51351e6d54bcc325
gers <geraquem@gmail.com>
2024-04-03 12:47:57 +0200

    wip

--- app/src/main/java/com/mmfsin/copixuelas/presentation/MainActivity.kt
+++ app/src/main/java/com/mmfsin/copixuelas/presentation/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
 
     private var mInterstitialAd: InterstitialAd? = null
 
-    var showWarningDialog = true
     var firstTime = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
